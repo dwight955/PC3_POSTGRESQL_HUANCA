b79a6e10929af45d7299b037cf48cd2d2a68fd07
Dwight Huanca <dwightht.anthony955@gmail.com>
2023-06-28 17:28:17 -0500

    Ajustes

--- src/main/java/com/cibertec/controller/EmpresaController.java
+++ src/main/java/com/cibertec/controller/EmpresaController.java
@@ -1,10 +1,14 @@
 package com.cibertec.controller;
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +24,7 @@ import com.cibertec.service.EmpresaService;
 
 @RestController
 @RequestMapping("/rest/Empresa")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmpresaController {
 
 	@Autowired
@@ -33,8 +38,16 @@ public class EmpresaController {
 
 	@PostMapping
 	public ResponseEntity<?> inserta(@RequestBody Empresa obj) {
+		// Valor por defecto
+		obj.setFlag(1);
+		Map<String, Object> salida = new HashMap<>();
 		Empresa objSalida = service.insertaEmpresa(obj);
-		return ResponseEntity.ok(objSalida);
+		if(objSalida != null) {
+			salida.put("mensaje", "Registro exitoso");
+		}else {
+			salida.put("mensaje", "Ocurrio un problema");
+		}
+		return ResponseEntity.ok(salida);
 	}
 
 	@GetMapping("/porId/{id}")
@@ -42,34 +55,40 @@ public class EmpresaController {
 		Optional<Empresa> optSalida = service.buscaEmpresa(id);
 		return ResponseEntity.ok(optSalida.get());
 	}
- 
-	
+
 	@GetMapping("/porId")
 	public ResponseEntity<?> listaPorIdEnParamaterer(
-			@RequestParam(name = "id", defaultValue = "0", required = true) Integer id) {
+		@RequestParam(name = "id", defaultValue = "0", required = true) Integer id) {
 		Optional<Empresa> optSalida = service.buscaEmpresa(id);
 		return ResponseEntity.ok(optSalida.get());
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> eliminaEmpresa(@PathVariable Integer id) {
+		Map<String, Object> salida = new HashMap<>();
 		service.eliminaEmpresa(id);
 		Optional<Empresa> optEmpresa = service.buscaEmpresa(id);
-		if (optEmpresa.isEmpty()) {
-			return ResponseEntity.ok("Eliminación exitosa");
+		if(optEmpresa.isEmpty()) {
+			salida.put("mensaje", "Eliminacion exitosa");
+		}else {
+			salida.put("mensaje", "Ocurrio un error");
 		}
-		return ResponseEntity.ok("No existe el id " + id);
+		return ResponseEntity.ok(salida);
 	}
 	
 	@PutMapping
 	public ResponseEntity<?> actualiza(@RequestBody Empresa obj) {
 		Optional<Empresa> optEmpresa = service.buscaEmpresa(obj.getIdEmpresa());
+		Empresa objSalida = null;
+		Map<String, Object> salida = new HashMap<>();
 		if (optEmpresa.isPresent()) {
-			Empresa objSalida = service.insertaEmpresa(obj);
-			return ResponseEntity.ok(objSalida);
+			objSalida = service.insertaEmpresa(obj);
+			salida.put("mensaje", "Se actualizo el ID --> "+obj.getIdEmpresa());
+			return ResponseEntity.ok(salida);
 		} else {
-			return ResponseEntity.ok("El Empresa no existe");
+			salida.put("mensaje", "Ocurrio un error");
 		}
+		return ResponseEntity.ok(salida);
 	}
 	
 	@GetMapping("/porTipoRiesgo/{id}")
